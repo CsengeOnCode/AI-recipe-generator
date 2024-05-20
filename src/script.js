40e88656b8f50ead816083b75f64e3a5972e87fc
CsengeOnCode <161061835+CsengeOnCode@users.noreply.github.com>
2024-05-20 15:23:32 +0100

    added the AI Api integration with axios

--- src/script.js
+++ src/script.js
@@ -1,12 +1,24 @@
+function displayRecipe(response) {
+  new Typewriter("#recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
 function generateRecipe(event) {
   event.preventDefault();
 
-  new Typewriter("#recipe", {
-    strings: "Tomato schrimps and a lot more fun",
-    autoStart: true,
-    delay: 50,
-    cursor: "",
-  });
+  let userInput = document.querySelector("#user-input");
+
+  let apiKey = "b6461bfaaot32c8f10f63ac1e954cc39";
+  let context =
+    "You are an italian chefe specialized in pasta. Provide a simple recipe. Please generate the recipe in basin HTML and always show the ingredients a list item and start the instructions in a new line";
+  let prompt = `generate a short recipe using the ingredient ${userInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let generatorFormElement = document.querySelector("#generator-form");
