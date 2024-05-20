e475e9b4e4794afa4a7bb569d5558bd2196e18f9
CsengeOnCode <161061835+CsengeOnCode@users.noreply.github.com>
2024-05-20 15:00:32 +0100

    Added basic form handling and Typewriter plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "Tomato schrimps and a lot more fun",
+    autoStart: true,
+    delay: 50,
+    cursor: "",
+  });
+}
+
+let generatorFormElement = document.querySelector("#generator-form");
+generatorFormElement.addEventListener("submit", generateRecipe);
